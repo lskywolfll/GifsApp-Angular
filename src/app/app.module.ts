be04db504dd71905f7d2bbfb113f7b07f7c412db
Rene <renealejandrosanchezmorales@gmail.com>
2021-09-14 22:48:24 -0300

    added http client module on app and gifsservice using httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {  NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, GifsModule],
+  imports: [BrowserModule, HttpClientModule, SharedModule, GifsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  private _apikey: string = '6vLJCHo1qdnTYUbYKz5KFbpxUwiTJN7x';
+  private _gifsLimit: number = 10;
+  private _gifsUrl: string = `https://api.giphy.com/v1/gifs/search?api_key=${this._apikey}&limit=${this._gifsLimit}`;
   private _historial: string[] = [];
 
   get historial() {
     return [...this._historial];
   }
+
+  constructor(private readonly http: HttpClient) {}
 
   buscarGifs(query: string) {
     query = query.trim().toLowerCase();
@@ -20,6 +26,9 @@ export class GifsService {
       alert("It's duplicate search");
       return;
     } else {
+      this.http.get(`${this._gifsUrl}&q=${query}`).subscribe(resp => {
+        console.log(resp);
+      })
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
     }
